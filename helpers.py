c3a5cfa2e515b9a64400a626fcc6aec869c2bcd7
narh <kpododanielnarh@gmail.com>
2021-01-08 17:06:10 +0000

    added falsy verification

--- helpers.py
+++ helpers.py
@@ -10,3 +10,11 @@ def check_existence(item, args):
     '''Check if item exists in list '''
     if item in args:
         return True
+
+
+def isEqual(a, b):
+    '''Determine if a == b '''
+    return a == b
+
+
+print(isEqual(10, 90))
--- test_helpers.py
+++ test_helpers.py
@@ -7,6 +7,9 @@ class TestHelper(unittest.TestCase):
     def setUp(self):
         self.items = [10, 20, 40]
 
+    def tearDown(self):
+        pass
+
     def test_aggregate(self):
         result = helpers.aggregate(self.items)
         self.assertEqual(result, 70)
@@ -14,6 +17,10 @@ class TestHelper(unittest.TestCase):
     def test_existence(self):
         result = helpers.check_existence(10, self.items)
         self.assertTrue(result, True)
+
+    def test_equality(self):
+        result = helpers.isEqual(100, 89)
+        self.assertFalse(result, False)
 
 
 unittest.main()
